5b6b0b3ef54f0133ef90bcdd6d4350c11f288ca2
SantiFagliano <fagliano.santi@gmail.com>
2021-05-14 21:07:05 -0300

    agregamos la base del layout

---
+++ GestorPedidos/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using GestorPedidos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestorPedidos.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LoginController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(int aux)
+        {
+            return View();
+        }
+
+    }
+}
