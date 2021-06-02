dad47938f464a1296d4e844f665f31c260e0d8bb
SantiFagliano <fagliano.santi@gmail.com>
2021-06-01 21:37:05 -0300

    add base of Order Service

---
+++ Servicios/OrderService.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace Service
+{
+    public class OrderService : IOrderService, IBaseService<Order>
+    {
+        public void Create(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteByid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Order entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
