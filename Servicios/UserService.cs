36bc1ee519bf7d908acd40f7eeb4cd1dfcbffa97
SantiFagliano <fagliano.santi@gmail.com>
2021-06-01 21:39:51 -0300

    add base of User Service

---
+++ Servicios/UserService.cs
@@ -0,0 +1,43 @@
+﻿using Models;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class UserService : IUserService, IBaseService<User>
+    {
+        public void Create(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteByid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
