bb2e9e51180852f69388c522d57d35904dbc4906
SantiFagliano <fagliano.santi@gmail.com>
2021-06-01 22:25:44 -0300

    feat: add item validation

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Models.ValidationCustom;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models
 {
     public class Item : IAuditable
     {
+        [Required]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [CustomOnlyNumber(ErrorMessage = "Por favor ingrese solo numeros")]
         public int Code { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
         public string Description { get; set; }
         public int CreatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public DateTime CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
@@ -13,6 +19,6 @@ namespace Models
         public DateTime UpdateDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int DeletedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public DateTime DeletedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool Delete { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Deleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
