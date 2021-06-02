dc29ddce73d29a7fbff3abf68bece5d4672a9902
SantiFagliano <fagliano.santi@gmail.com>
2021-06-01 22:25:32 -0300

    feat: add client validation

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Models.ValidationCustom;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +10,26 @@ namespace Models
 {
     public class Client : IAuditable
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [CustomOnlyString(ErrorMessage = "Por favor no ingrese caracteres especiales.")]
         public string Name { get; set; }
+
+        [CustomOnlyNumber(ErrorMessage ="Por favor ingrese solo numeros")]
         public int Number { get; set; }
+
+        [CustomOnlyNumber(ErrorMessage = "Por favor ingrese solo numeros")]
+        [MinLength(8, ErrorMessage = "Por favor ingrese al menos 8 digitos")]
+        [MaxLength(10, ErrorMessage = "Por favor ingrese como mucho 10 digitos")]
         public int PhoneNumber { get; set; }
+
+        [EmailAddress(ErrorMessage = "Por favor ingrese un formato de email valido.")]
         public string Email { get; set; }
         public string Direccion { get; set; }
+
+        [CustomOnlyNumber(ErrorMessage = "Por favor ingrese solo numeros")]
         public int Cuit { get; set; }
         public int CreatedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public DateTime CreatedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
@@ -21,6 +37,6 @@ namespace Models
         public DateTime UpdateDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int DeletedBy { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public DateTime DeletedDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool Delete { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Deleted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
