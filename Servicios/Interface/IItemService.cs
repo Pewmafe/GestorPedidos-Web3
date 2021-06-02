854c6518c5a00234ca2ab0f058543dc59d57df15
SantiFagliano <fagliano.santi@gmail.com>
2021-06-01 21:36:43 -0300

    add base of Item Service

--- Servicios/Interface/IItemService.cs
+++ Servicios/Interface/IItemService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Servicios
+namespace Service
 {
-    interface IArticuloService
+    public interface IItemService
     {
     }
 }
---
+++ Servicios/ItemService.cs
@@ -0,0 +1,42 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ItemService : IItemService, IBaseService<Item>
+    {
+        public void Create(Item entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Item entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteByid(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Item entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
