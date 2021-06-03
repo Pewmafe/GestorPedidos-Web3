d051bbae7deb78248600bac67c371b6ed84f275f
sanchez matias <matias-leandro@outlook.com>
2021-06-02 23:06:32 -0300

    validationConstants class created

---
+++ Models/Constants/ValidationConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Constants
+{
+    class ValidationConstants
+    {
+        public const string RequiredField= "Este campo es requerido";
+        public const string OnlyString = "Por favor no ingrese caracteres especiales.";
+        public const string OnlyNumber = "Por favor ingrese solo numeros";
+        public const string MinLengthEightDigits = "Por favor ingrese al menos 8 digitos";
+        public const string MaxLengthTenDigits = "Por favor ingrese un maximo de 10 digitos";
+        public const string InvalidEmailFormat = "Por favor ingrese un formato de email valido.";
+        public const string InvalidDate = "La fecha no debe ser mayor al dia de hoy";
+    }
+}
