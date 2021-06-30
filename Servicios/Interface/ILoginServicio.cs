284e473b01a17fdffd32ef9da4965a44a16dc276
sanchez matias <matias-leandro@outlook.com>
2021-06-29 22:37:42 -0300

    loginServicio created

---
+++ Servicios/Interface/ILoginServicio.cs
@@ -0,0 +1,16 @@
+﻿using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Service.Interface
+{
+    public interface ILoginServicio
+    {
+        Usuario LogIn(Usuario usuario);
+        void LogOut(HttpContext httpContext);
+    }
+}
