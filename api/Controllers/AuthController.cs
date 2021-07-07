4947688d9ca8348e4f077bfd78bc9391c707d5c7
sanchez matias <matias-leandro@outlook.com>
2021-07-06 22:08:20 -0300

    basic login created

---
+++ api/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using api.Helper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Models.Models;
+using Service;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+
+
+        private ILoginServicio loginServicio;
+        private _20211CTPContext dbContext;
+        private readonly IConfiguration conf;
+
+        public AuthController(_20211CTPContext _dbContext, IConfiguration config)
+        {
+            this.dbContext = _dbContext;
+            this.loginServicio = new LoginService(dbContext);
+            this.conf = config;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public ActionResult<object> Login(Usuario usuario)
+        {
+            string secret = this.conf.GetValue<string>("Secret");
+            JWTHelper jwtHelper = new JWTHelper(secret);
+            string token = jwtHelper.CreateToken(usuario.Email);
+
+            return Ok(new
+            {
+                ok=true,
+                msg="Logueado con exito.",
+                token
+            });
+        }
+
+        /* [HttpPost]
+         [Route("/logout")]
+         public object Logout()
+         {
+
+         }*/
+    }
+}
