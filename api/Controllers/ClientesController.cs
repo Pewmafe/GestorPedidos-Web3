970e0ef30c94d5b77a41bb4892c7bdc5773e17bd
sanchez matias <matias-leandro@outlook.com>
2021-07-06 21:06:36 -0300

    changed class name

---
+++ api/Controllers/ClientesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Models;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientesController : Controller
+    {
+        private IClienteServicio clienteServicio;
+        private _20211CTPContext dbContext;
+
+        public ClientesController(_20211CTPContext _dbContext)
+        {
+            this.dbContext = _dbContext;
+            this.clienteServicio = new ClienteServicio(dbContext);
+        }
+
+        [HttpGet]
+        public IEnumerable<Cliente> GetAll()
+        {
+            List<Cliente> clientes = clienteServicio.ListarTodos();
+
+            return clientes;
+        }
+    }
+}
