df31b1f789c9f0c35bb17ffc35df86c1ebb89906
SantiFagliano <fagliano.santi@gmail.com>
2021-07-12 17:15:24 -0300

    agregamos la vista de error

--- GestorPedidos/Controllers/HomeController.cs
+++ GestorPedidos/Controllers/HomeController.cs
@@ -1,33 +1,20 @@
-﻿using GestorPedidos.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace GestorPedidos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult ErrorPage()
         {
+            if (TempData["errorException"] != null) ViewData["errorException"] = TempData["errorException"];
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
